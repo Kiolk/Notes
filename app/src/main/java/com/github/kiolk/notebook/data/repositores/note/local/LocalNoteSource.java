4405c403a7d826e27897ba55d5e5f2cf653886a7
Yauheni <tyteishi@gmail.com>
2019-11-27 00:38:33 +0300

    Removed implementation added TODOs

--- app/src/main/java/com/github/kiolk/notebook/data/repositores/note/local/LocalNoteSource.java
+++ app/src/main/java/com/github/kiolk/notebook/data/repositores/note/local/LocalNoteSource.java
@@ -1,14 +1,11 @@
 package com.github.kiolk.notebook.data.repositores.note.local;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 
 import com.github.kiolk.notebook.data.DBHelper;
 import com.github.kiolk.notebook.data.models.Note;
 import com.github.kiolk.notebook.data.repositores.note.NoteDataSource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LocalNoteSource implements NoteDataSource {
@@ -22,40 +19,22 @@ public class LocalNoteSource implements NoteDataSource {
 
     @Override
     public void insertNote(Note note) {
-        ContentValues cv = new ContentValues();
-        cv.put(Note.COLUMN_TITLE, note.getTitle());
-        cv.put(Note.COLUMN_TEXT, note.getText());
-        helper.getReadableDatabase().insert(Note.TABLE_NAME, null, cv);
+        //TODO
     }
 
     @Override
     public void updateNote(Note note) {
-        ContentValues cv = new ContentValues();
-        cv.put(Note.COLUMN_ID, note.getId());
-        cv.put(Note.COLUMN_TITLE, note.getTitle());
-        cv.put(Note.COLUMN_TEXT, note.getText());
-        helper.getReadableDatabase().update(Note.TABLE_NAME, cv, Note.COLUMN_ID + " = ?", new String[]{note.getId().toString()} );
+        //TODO
     }
 
     @Override
     public void deleteNote(long id) {
-        helper.getReadableDatabase().delete(Note.TABLE_NAME, Note.COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
+        //TODO
     }
 
     @Override
     public List<Note> getAllNotes() {
-
-        Cursor cursor = helper.getReadableDatabase().rawQuery("SELECT * FROM " + Note.TABLE_NAME, null);
-
-        List<Note> notes = new ArrayList<>();
-        while (cursor.moveToNext()){
-            Note note = new Note();
-            note.setId(cursor.getLong(cursor.getColumnIndex(Note.COLUMN_ID)));
-            note.setTitle(cursor.getString(cursor.getColumnIndex(Note.COLUMN_TITLE)));
-            note.setText(cursor.getString(cursor.getColumnIndex(Note.COLUMN_TEXT)));
-            notes.add(note);
-        }
-
-        return notes;
+        //TODO
+        return null;
     }
 }
